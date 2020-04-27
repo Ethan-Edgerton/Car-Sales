5046503e5cd25a17bd18447d024ffe4aee79d500
Ethan-Edgerton <Ethan.E.Edgerton@gmail.com>
2020-04-27 17:10:23 -0500

    MVP met

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "bulma/css/bulma.css";
+import "./styles.scss";
 
-import 'bulma/css/bulma.css';
-import './styles.scss';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { reducer } from "./reducers/reducer";
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+const rootElement = document.getElementById("root");
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+
+  rootElement
+);
